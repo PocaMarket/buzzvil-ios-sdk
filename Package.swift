b22624783d407279b9f83dd1f7424137d8d5f088
Daniel Song <68672528+Minkwan-Song@users.noreply.github.com>
2024-04-08 19:16:16 +0900

    5.9.1

--- Package.swift
+++ Package.swift
@@ -32,23 +32,23 @@ let package = Package(
     ),
     .binaryTarget(
       name: "BuzzvilSDK",
-      url: "",
-      checksum: ""
+      url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/12632/BuzzvilSDK.zip",
+      checksum: "5c1378f9d0f9116eb7104451481d07c184964ff41f9d892fe7bbc473b7b27777"
     ),
     .binaryTarget(
       name: "BuzzAdBenefitSDK",
-      url: "",
-      checksum: ""
+      url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/12632/BuzzAdBenefitSDK.zip",
+      checksum: "e3599f799c1a4cd2f93ffd8c5d099ce7fed42a74812a9cc661e8dac5a8cb9062"
     ),
     .binaryTarget(
       name: "BuzzBoosterSDK",
-      url: "",
-      checksum: ""
+      url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/12632/BuzzBoosterSDK.zip",
+      checksum: "c56a882d5f7e64970a33aaef06b03ae4c103db9d99a4b8c3b606aaca3c99419c"
     ),
     .binaryTarget(
       name: "BuzzRxSwift",
-      url: "",
-      checksum: ""
+      url: "https://github.com/Buzzvil/RxSwift/releases/download/v7.0.0/BuzzRxSwift.zip",
+      checksum: "772edfb6e77b41f888d4430e46e00d1ec147dc9d26b3f2ce464dbc00d678f963"
     ),
   ],
   swiftLanguageVersions: [
