f79a1282ca21bf63879dfe98e51ae8c95c5bb933
Daniel Song <68672528+Minkwan-Song@users.noreply.github.com>
2024-11-12 12:41:51 +0900

    5.19.2

--- Package.swift
+++ Package.swift
@@ -15,7 +15,9 @@ let package = Package(
       ]
     ),
   ],
-  dependencies: [],
+  dependencies: [
+    .package(url: "https://github.com/googleads/swift-package-manager-google-interactive-media-ads-ios", .upToNextMajor(from: "3.23.0")),
+  ],
   targets: [
     .target(
       name: "BuzzvilSDKTarget",
@@ -24,23 +26,24 @@ let package = Package(
         "BuzzAdBenefitSDK",
         "BuzzBoosterSDK",
         "BuzzRxSwift",
+        .product(name: "GoogleInteractiveMediaAds", package: "swift-package-manager-google-interactive-media-ads-ios"),
       ],
       path: "Sources/BuzzvilSDKTarget"
     ),
     .binaryTarget(
       name: "BuzzvilSDK",
-      url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/15792/BuzzvilSDK.zip",
-      checksum: "a982939fb7be0bd2a27f3ac401f42eff93f27e4002fd4215c8b94ec17259732c"
+      url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/15790/BuzzvilSDK.zip",
+      checksum: "c9a4258bea31cc83b50db997c174b692e4ed81f69cf31ec6896547e3bb8f8924"
     ),
     .binaryTarget(
       name: "BuzzAdBenefitSDK",
-      url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/15792/BuzzAdBenefitSDK.zip",
-      checksum: "83a5569c176a364e0f28b2568d2fe750dba0cbc6ecc1ee285488104d6346e4fa"
+      url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/15790/BuzzAdBenefitSDK.zip",
+      checksum: "2c6fc2fdafa6ee886619dd3f619bd0ac9ace51a736b56718a8b3c31a1fa12878"
     ),
     .binaryTarget(
       name: "BuzzBoosterSDK",
-      url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/15792/BuzzBoosterSDK.zip",
-      checksum: "217ef695154cccb7d0187ee53f1758661ca1f31b2bc31daac9a0f963f361875d"
+      url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/15790/BuzzBoosterSDK.zip",
+      checksum: "945ca0400527bec54a129326360b638be66d4e19c3a3bd06862b9e5d1089ff70"
     ),
     .binaryTarget(
       name: "BuzzRxSwift",
